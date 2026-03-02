d38cedb3559f169a4b87739f4872b297735cef96
coehgns <modooboiroo@gmail.com>
2026-03-02 22:30:49 +0900

    first commit

---
+++ src/test/java/org/example/dsm/MentoringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
